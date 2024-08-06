a3223434f55b1f603a08367d5769179d15701709
Anastasiia <anastasiia@cinnamontoast.ca>
2024-08-06 15:52:34 -0400

    homepage bugherd sweep

--- js/hp-slider.js
+++ js/hp-slider.js
@@ -46,38 +46,3 @@ function showDivsLaptop(j) {
     laptopItems[slideIndexLaptop-1].style.display = "flex";
     dots[slideIndexLaptop-1].className += " w3-opacity-off";
 }        
-
-// mobile testimonial slider script
-// var slideIndex = 1;
-// showDivs(slideIndex);
-
-// function plusDivs(n) {
-//     showDivs(slideIndex += n);
-// }
-
-// function showDivs(n) {
-//     var i;
-//     var x = document.getElementsByClassName("hp-slider-item-mobile");
-//     if (n > x.length) {slideIndex = 1}
-//     if (n < 1) {slideIndex = x.length}
-//     for (i = 0; i < x.length; i++) {
-//         x[i].style.display = "none";
-//     }
-//     x[slideIndex-1].style.display = "block";  
-// }
-
-// // keyboard accessibility
-// document.addEventListener('keydown', function(event) {
-//     if (event.key === 'ArrowLeft') {
-//         plusDivs(-1); // previous slide
-//     } else if (event.key === 'ArrowRight') {
-//         plusDivs(1); // next slide
-//     }
-// });
-// document.addEventListener('keydown', function(event) {
-//     if (event.key === 'ArrowLeft') {
-//         showDivsLaptop(slideIndexLaptop - 1); // previous slide
-//     } else if (event.key === 'ArrowRight') {
-//         showDivsLaptop(slideIndexLaptop + 1); // next slide
-//     }
-// });
