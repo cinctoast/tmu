605175c6a2257f3aa50866abde4bd67b16f6d4b4
Anastasiia <anastasiia@cinnamontoast.ca>
2024-08-06 18:01:21 -0400

    QC 1.2

--- js/hp-slider.js
+++ js/hp-slider.js
@@ -7,6 +7,8 @@ var thumbnail1 = document.getElementById("thumbnail-1");
 var thumbnail2 = document.getElementById("thumbnail-2");
 var thumbnail3 = document.getElementById("thumbnail-3");
 
+
+
 thumbnail1.style.display = "none";
 
 function showDivsLaptop(j) {
@@ -46,3 +48,21 @@ function showDivsLaptop(j) {
     laptopItems[slideIndexLaptop-1].style.display = "flex";
     dots[slideIndexLaptop-1].className += " w3-opacity-off";
 }        
+
+thumbnail1.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+        currentDiv(1);
+    }
+});
+
+thumbnail2.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+        currentDiv(2);
+    }
+});
+
+thumbnail3.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+        currentDiv(3);
+    }
+});
