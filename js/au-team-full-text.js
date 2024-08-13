7c4b71ec8d60070183bfb386d65374fd971470ab
Anastasiia <anastasiia@cinnamontoast.ca>
2024-08-13 16:05:18 -0400

    courses QC

--- js/au-team-full-text.js
+++ js/au-team-full-text.js
@@ -1,10 +1,8 @@
-if (window.innerWidth < 1100) {
-    document.querySelectorAll('.au-team-member-full').forEach((btn, index) => {
-        btn.addEventListener('click', () => {
-            var textDiv = document.querySelectorAll('.au-team-member-text')[index];
-            textDiv.style.height = 'auto';
-            textDiv.style.paddingBottom = '25px';
-            btn.style.display = 'none';
-        });
+document.querySelectorAll('.au-team-member-full').forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        var textDiv = document.querySelectorAll('.au-team-member-text')[index];
+        textDiv.style.height = 'auto';
+        textDiv.style.paddingBottom = '25px';
+        btn.style.display = 'none';
     });
-}
+});
